013e422e61dec7314b6b84be0471c4a87c586d0b
izyyhar <112462565+izyyhar@users.noreply.github.com>
2022-10-28 15:31:54 +0800

    test1

---
+++ hellospringweb/src/main/java/com/cognixia/HellospringwebApplication.java
@@ -0,0 +1,15 @@
+package com.cognixia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class HellospringwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellospringwebApplication.class, args);
+	}
+
+}
